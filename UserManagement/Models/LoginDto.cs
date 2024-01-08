221835d284d7b03b4fdbf1b87c1c5b9725e008e7
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-08 20:32:01 +0100

    models

---
+++ UserManagement/Models/LoginDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models
+{
+    public class LoginDto
+    {
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ UserManagement/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.Models
+{
+    public class User
+
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+    }
+}
