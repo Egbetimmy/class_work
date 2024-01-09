7c688806b30d02c67771172d010b4bd1e1c8f10a
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-09 10:00:10 +0100

    helpers

---
+++ UserManagement/Helper/PhoneNumberValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserManagement.Helpers
+{
+    public static class PhoneNumberValidator
+    {
+        public static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            // Regular expression to match a simple phone number format (e.g., XXX-XXX-XXXX)
+            var regex = new Regex(@"^\d{3}-\d{3}-\d{4}$");
+
+            return regex.IsMatch(phoneNumber);
+        }
+    }
+}
