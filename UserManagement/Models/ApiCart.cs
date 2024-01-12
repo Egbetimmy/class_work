8643ebb2ce6e14e699c5084c6ea9325d26b9aa6b
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-12 12:08:25 +0100

    models

---
+++ UserManagement/Models/ApiCart.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace UserManagement.Models
+{
+    public class ApiCart
+    {
+        /*var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, "https://dummyjson.com/carts");
+        var response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+        */
+        public async Task<Cart> CartAsync(int id)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://dummyjson.com/carts/{id}");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+            if (response.Content != null)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                if (content != null)
+                {
+                    var myDeserializedClass = JsonConvert.DeserializeObject<Cart>(content);
+
+                    return myDeserializedClass;
+                }
+            }
+            return null;
+
+            //var options = new RestClientOptions("https://dummyjson.com")
+            //{
+            //    MaxTimeout = -1,
+            //};
+            //var client = new RestClient(options);
+            //var request = new RestRequest("/products/1", Method.Get);
+            //RestResponse response = await client.ExecuteAsync(request);
+            //Console.WriteLine(response.Content);
+        }
+
+        public async Task<AllRoot> AllCartsAsync()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://dummyjson.com/carts");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+
+            if (response.Content != null)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                if (content != null)
+                {
+                    var myDeserializedClass = JsonConvert.DeserializeObject<AllRoot>(content);
+
+                    return myDeserializedClass;
+                }
+            }
+
+            return null;
+
+            //var options = new RestClientOptions("https://dummyjson.com")
+            //{
+            //    MaxTimeout = -1,
+            //};
+            //var client = new RestClient(options);
+            //var request = new RestRequest("/products/1", Method.Get);
+            //RestResponse response = await client.ExecuteAsync(request);
+            //Console.WriteLine(response.Content);
+        }
+    }
+
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Cart
+    {
+        public int id { get; set; }
+        public List<Product> products { get; set; }
+        public int total { get; set; }
+        public int discountedTotal { get; set; }
+        public int userId { get; set; }
+        public int totalProducts { get; set; }
+        public int totalQuantity { get; set; }
+    }
+
+    public class Product
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }
+        public int total { get; set; }
+        public double discountPercentage { get; set; }
+        public int discountedPrice { get; set; }
+        public string thumbnail { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Cart> carts { get; set; }
+        public int total { get; set; }
+        public int skip { get; set; }
+        public int limit { get; set; }
+    }
+}
+
---
+++ UserManagement/Models/Session.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace UserMangement.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CreatedBy { set; get; }
+        public DateTime CreatedAt { set; get; }
+        public List<Department> Departments { get; set; }
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { set; get; }
+        public int SessionId { get; set; }
+        //public Session Session{ get; set; }
+    }
+
+    public class SessionPostDto
+    {
+        public string Name { get; set; }
+    }
+    public class DepartmentPostDto
+    {
+        public string Name { get; set; }
+        public int SessionId { set; get; }
+    }
+    public class SessionRequestDto
+    {
+        public string Name { get; set; }
+        public List<DepartmentRequestDto> Departments { set; get; }
+    }
+    public class DepartmentRequestDto
+    {
+        public string Name { get; set; }
+    }
+
+    public class SessionDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<DepartmentDto> Departments { get; set; }
+    }
+
+    public class DepartmentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SessionId { get; set; }
+    }
+}
+
