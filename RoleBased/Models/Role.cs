e745f804cd9cb5c16427939ca2d00a97291d4976
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-16 15:37:36 +0100

    rb

---
+++ RoleBased/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿namespace RoleBased.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; } // Navigation property for users
+    }
+
+    public class RoleDTO
+    {
+        public string RoleName { get; set; }
+    }
+}
---
+++ RoleBased/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace RoleBased.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public List<Role> Roles { get; set; } // Navigation property for roles
+    }
+
+    public class UserLoginDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
