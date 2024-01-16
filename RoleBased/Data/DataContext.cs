57f2e4835bef23897395b5395088fac5fed6b438
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-16 15:37:13 +0100

    rb

---
+++ RoleBased/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoleBased.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure your entity relationships, keys, etc. here if needed
+        // For example, if you have a many-to-many relationship between User and Role:
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Roles)
+            .WithMany(r => r.Users)
+            .UsingEntity(j => j.ToTable("UserRoles"));
+    }
+}
