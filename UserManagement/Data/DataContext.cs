64f0bf49cc75b1035b506bbf8d0de0289b8aadea
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-25 12:10:00 +0100

    updated

--- UserManagement/Data/DataContext.cs
+++ UserManagement/Data/DataContext.cs
@@ -14,3 +14,6 @@ namespace UserManagement.Data
         public DbSet<Department> departments { get; set; }
     }
 }
+/*{
+  "message": "User registered successfully. Please check your email for verification. eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImVnYmV0aWltbXlAZ21haWwuY29tIiwibmFtZWlkIjoiMCIsIk9UUCI6Ijk0OTQwMSIsIlBhc3N3b3JkIjoia2VsZSIsIkZpcnN0TmFtZSI6InN0cmluZyIsIkxhc3ROYW1lIjoiJDJhJDEwJEI0dXROR2UxQlV2SGRqb0tFNzF1dnViL05mc3h6Z3BsZnVmYWxSdVBQLkd3dkM3cDF4ajh1IiwibmJmIjoxNzA1OTE0NDQ1LCJleHAiOjE3MDU5MTgwNDUsImlhdCI6MTcwNTkxNDQ0NX0.RwgveUIMQ3dXdBfpiSYCEezrWoFUMaY-EdGa0KRDT0o"
+}*/
