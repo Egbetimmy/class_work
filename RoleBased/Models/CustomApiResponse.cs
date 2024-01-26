4cd675a926a89b147fac058a174519caf7f479c2
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-26 12:48:13 +0100

    model

---
+++ RoleBased/Models/CustomApiResponse.cs
@@ -0,0 +1,20 @@
+﻿public class CustomApiResponse
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+
+    public CustomApiResponse(bool success, string message)
+    {
+        Success = success;
+        Message = message;
+    }
+
+    public string GetResponseAsString()
+    {
+        return $"Success: {Success}, Message: {Message}";
+    }
+}
+
+
+//var customResponse = new CustomApiResponse(true, "Operation successful");
+//Console.WriteLine(customResponse.GetResponseAsString());
