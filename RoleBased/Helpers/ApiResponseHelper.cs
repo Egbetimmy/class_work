64b9c961fb50de6374c07ee4c38af90da0deb0ab
Egbe Timmy <88975749+Egbetimmy@users.noreply.github.com>
2024-01-28 17:01:12 +0100

    helper

---
+++ RoleBased/Helpers/ApiResponseHelper.cs
@@ -0,0 +1,35 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json; // Make sure to include the correct using statement
+
+//public static class ApiResponseHelper
+//{
+//    public static IActionResult CreateSuccessResponse(string message = "Operation successful")
+//    {
+//        var customResponse = new CustomApiResponse(true, message);
+//        return new ObjectResult(customResponse)
+//        {
+//            StatusCode = 200 // You can customize the status code for success
+//        };
+//    }
+
+//    public static string GetSuccessResponseAsString(string message = "Operation successful")
+//    {
+//        var customResponse = new CustomApiResponse(true, message);
+//        return JsonConvert.SerializeObject(customResponse);
+//    }
+
+//    public static IActionResult CreateErrorResponse(string message = "An error occurred")
+//    {
+//        var customErrorResponse = new CustomApiResponse(false, message);
+//        return new ObjectResult(customErrorResponse)
+//        {
+//            StatusCode = 500 // You can customize the status code for errors
+//        };
+//    }
+
+//    public static string GetErrorResponseAsString(string message = "An error occurred")
+//    {
+//        var customErrorResponse = new CustomApiResponse(false, message);
+//        return JsonConvert.SerializeObject(customErrorResponse);
+//    }
+//}
---
+++ RoleBased/Helpers/GenerateOTP.cs
@@ -0,0 +1,15 @@
+﻿namespace BITMAS.Helper
+{
+    public class GenerateOTP
+    {
+        public static string OTP()
+        {
+            // Generate a random 6-digit OTP (you can adjust the length as needed)
+            Random rand = new Random();
+            int otpValue = rand.Next(100000, 999999); // 6-digit OTP
+            string otp = otpValue.ToString();
+
+            return otp;
+        }
+    }
+}
